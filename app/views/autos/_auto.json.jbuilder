b05bf915fc76f9d224a84db8b1791af935fc3813
lupita <lupita.hernndeztovar@gmail.com>
2017-06-07 11:22:16 -0500

    primer commit

---
+++ app/views/autos/_auto.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! auto, :id, :marca, :anio, :precio, :fecha_adquisicion, :created_at, :updated_at
+json.url auto_url(auto, format: :json)
---
+++ db/migrate/20170601171029_create_autos.rb
@@ -0,0 +1,12 @@
+class CreateAutos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :autos do |t|
+      t.string :marca
+      t.integer :anio
+      t.float :precio
+      t.date :fecha_adquisicion
+
+      t.timestamps
+    end
+  end
+end
